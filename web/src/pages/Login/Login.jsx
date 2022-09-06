5b1572b1735d58d45646526710fdc4e13bfcd7d4
Gustavo Nascimento Souza <gustavo.ns@outlook.com.br>
2022-09-06 00:03:53 -0300

    :bookmark: 0.0.4 - :sparkles: Introduce a new Feature (SaSS/ScSS) :see_no_evil: Update .gitignore (Added all .map files from ScSS); :art: Improved the Code Format from Login.jsx; :lipstick: Modify all CSS structure with SaSS

--- web/src/pages/Login/Login.jsx
+++ web/src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import './Login.css'
-import {useState} from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
@@ -9,114 +9,114 @@ export default function Login() {
     //6 - controlled inputs
     // 3 - gerenciamento de dados 
 
-const [name, setName] = useState("");
-const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
 
 
-const handleName = (e) => {
-   setName(e.target.value);
-}
-// console.log(name);
-// console.log(email);
-
-const handleSubmit = (event) => {
-
-    //validação
-     //envio 
-
-    event.preventDefault();
-    console.log("Enviando o formulario");
-    console.log(name, password);
-
-
-    if(name !== 'aluno' || password !== '12345'){
-        
-    }else{
-        alert("LOGADO COM SUCESSO");
-        navigate("/matricula");
-       
+    const handleName = (e) => {
+        setName(e.target.value);
     }
+    // console.log(name);
+    // console.log(email);
 
-    // 7 - limpar formularios 
-    setName("");
-    setPassword("");
-    // setBio("");
-    // setRole("");
-  
-}
+    const handleSubmit = (event) => {
+
+        //validação
+        //envio 
+
+        event.preventDefault();
+        console.log("Enviando o formulario");
+        console.log(name, password);
+
+
+        if (name !== 'aluno' || password !== '12345') {
+
+        } else {
+            alert("Login realizado com sucesso");
+            navigate("/matricula");
+
+        }
+
+        // 7 - limpar formularios 
+        setName("");
+        setPassword("");
+        // setBio("");
+        // setRole("");
+
+    }
     return (
-        <div class='container'>
-            <div class='form-container'>
-                <div class='signin-signup'>
+        <div className='container'>
+            <div className='form-container'>
+                <div className='signin-signup'>
 
-                    <form class='sign-in-form' id='form' name='form' onSubmit={handleSubmit}>
-                        <h2 class='title'>
+                    <form className='sign-in-form' id='form' name='form' onSubmit={handleSubmit}>
+                        <h2 className='title'>
                             Login <span>-</span> Aluno
                         </h2>
 
-                        <div class='input-field'>
-                            <i class='fas fa-user'></i>
-                            <input type='text' placeholder='Usuário' id='login' name='login' required onChange={handleName}/>
+                        <div className='input-field'>
+                            <i className='fas fa-user'></i>
+                            <input type='text' placeholder='Usuário' id='login' name='login' required onChange={handleName} />
                         </div>
 
-                        <div class='input-field'>
-                            <i class='fas fa-lock'></i>
-                            <input type='password' placeholder='Senha' id='password' name='password' required  onChange={(e) => setPassword(e.target.value)}/>
+                        <div className='input-field'>
+                            <i className='fas fa-lock'></i>
+                            <input type='password' placeholder='Senha' id='password' name='password' required onChange={(e) => setPassword(e.target.value)} />
                         </div>
-                        <a class='form-a' href='#'>Esqueceu sua senha?</a>
-                        <input type='submit' value='Login' class='btn solid' />
+                        <a className='form-a' href='#'>Esqueceu sua senha?</a>
+                        <input type='submit' value='Login' className='btn solid' />
                     </form>
 
-                    <form action='#' class='sign-up-form'>
-                        <h2 class='title'>
+                    <form action='#' className='sign-up-form'>
+                        <h2 className='title'>
                             Login <span>-</span> Professor
                         </h2>
 
-                        <div class='input-field'>
+                        <div className='input-field'>
 
-                            <i class='fas fa-user'></i>
+                            <i className='fas fa-user'></i>
                             <input type='text' placeholder='User' />
                         </div>
 
-                        <div class='input-field'>
+                        <div className='input-field'>
 
-                            <i class='fas fa-lock'></i>
+                            <i className='fas fa-lock'></i>
                             <input type='password' placeholder='Senha' />
                         </div>
-                        <a class='form-a' href='#'>Esqueceu sua senha?</a>
-                        <input type='submit' value='Login' class='btn solid' />
+                        <a className='form-a' href='#'>Esqueceu sua senha?</a>
+                        <input type='submit' value='Login' className='btn solid' />
                     </form>
                 </div>
             </div>
 
-            <div class='panels-container'>
-                <div class='panel left-panel'>
-                    <div class='content'>
+            <div className='panels-container'>
+                <div className='panel left-panel'>
+                    <div className='content'>
                         <h3>É um professor?</h3>
                         <p>Está do lado errado desse form, só clicar no botão que será levado ao local correto
                         </p>
 
-                        <button class='btn transparent' id='sign-up-btn' onClick={modifyLogin}>
+                        <button className='btn transparent' id='sign-up-btn' onClick={modifyLogin}>
                             Login - Prof.
                         </button>
                     </div>
 
-                    <img class='image' src='./img/bg3.svg' alt='Get in the rocket' />
+                    <img className='image' src='./img/bg3.svg' alt='Get in the rocket' />
                 </div>
 
 
-                <div class='panel right-panel'>
-                    <div class='content'>
+                <div className='panel right-panel'>
+                    <div className='content'>
                         <h3>É um aluno??</h3>
                         <p>Está do lado errado desse form, só clicar no botão que será levado ao local correto
                         </p>
 
-                        <button class='btn transparent' id='sign-in-btn'>
+                        <button className='btn transparent' id='sign-in-btn'>
                             Login - Aluno
                         </button>
                     </div>
 
-                    <img class='image' src='./img/register.svg' alt='Get in the rocket' />
+                    <img className='image' src='./img/register.svg' alt='Get in the rocket' />
                 </div>
             </div>
         </div>
