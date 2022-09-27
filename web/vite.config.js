8ccb899823a9fbdb5ba972b87ee57c56128bdff6
Vicenzzo Toth Amorim <vicenzzo.toth@gmail.com>
2022-09-27 17:34:20 -0300

    adding dist

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/New-Education-System/'
+  base: '/New-Education-System/',
+  plugins: [react()]
+  
 })
